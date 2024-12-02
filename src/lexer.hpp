33239afacecb90b28e560e84cc2e435a43a036e3
Nino Keravel <61903425+riioze@users.noreply.github.com>
2024-12-02 20:18:05 +0100

    Update the documentation for parser.hpp

--- src/lexer.hpp
+++ src/lexer.hpp
@@ -5,8 +5,14 @@
 
 //! \file lexer.hpp
 
-//! \brief transforms a string into a vector of tokens
+//! \fn Node get_node(const std::string &str)
+//! \param str : the string to tranform to Node
+//! \return the Node corresponding to the str
+Node get_node(const std::string &str);
+
+//! \fn std::vector<Node> tokenize(std::string const &str)
 //! \param str : The string to tokenize
+//! \return The list of tokens corresponding to the str
 std::vector<Node> tokenize(std::string const &str);
 
 #endifs
--- src/parser.hpp
+++ src/parser.hpp
@@ -6,8 +6,30 @@
 #define PARSER_H
 //! \file parser.hpp
 
+//! \fn int find_last(std::vector<Node> const node_list,Node n)
+//! \param node_list : the list to loop threw to find n
+//! \param n : the Node to find in the list
+//! \return The last index in which n can be found, -1 else
+int find_last(std::vector<Node> const node_list,Node n);
+
+//! \fn void raise_missmatched_containers(std::string msg)
+//! \brief called when the containers (parenthesis ect) are missmatched (no opening parent for a closing ect)
+//! \param msg : message to add to the error
+void raise_missmatched_containers(std::string msg);
+
+//! \fn void group_by_containers(Node &node)
+//! \brief recursivly groups the containers and create the parse trees inside of each container
+//! \param node : Node in wich the containors will be matched and grouped
+void group_by_containers(Node &node);
+
+//! \fn void group_operators(Node &node)
+//! \brief recursivly goups the operators in a node following the priority rules
+//! \param node : Node in wich the operators will be matched and grouped
+void group_operators(Node &node);
+
 //! \fn Node get_parse_tree(std::vector<Node> node_list)
+//! \brief recursivly create a parse tree from a list of Node
+//! \param node_list : the list of Node from wich the parse tree will be created
 //! \return the tree created from a list of nodes
 Node get_parse_tree(std::vector<Node> node_list);
-
 #endif
